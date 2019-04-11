cc826df63d1e94be88accb853fc538ff4a20c6a6
Adam <adam1brownell@gmail.com>
2019-04-11 12:33:20 -0700

    interactive resume v1

---
+++ resume.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $("#data_click").click(function () {
+    $(".data_nc").toggleClass('data_c');
+    });
+
+  $("#product_click").click(function () {
+    $(".product_nc").toggleClass('product_c');
+    });
+
+  $("#research_click").click(function () {
+    $(".research_nc").toggleClass('research_c');
+    });
+  });
