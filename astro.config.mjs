889a05570cc9bd454135e9402b45d846c806cef1
Adam Brownell <adam1brownell@gmail.com>
2025-05-24 09:17:34 -0700

    add custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://github.com/adam1brownell/adam1brownell.github.io',
-  base: 'adam1brownell.github.io',
-  outDir: 'dist',
+  site: 'https://adambrownell.com',
+  base: '/',
 });
 
