ca71062798910476959243aad80392321cf825ee
adam1brownell <adam1brownell@gmail.com>
2025-06-09 13:33:28 -0700

    move to /myblog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 export default defineConfig({
   site: 'https://adambrownell.com',
-  base: '/',
+  base: '/myblog',
   hooks: {
     'astro:build:done': () => {
       fs.copyFileSync('public/CNAME', 'dist/CNAME');
